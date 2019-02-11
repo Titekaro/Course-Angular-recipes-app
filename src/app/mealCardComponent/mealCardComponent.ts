8430b424808cfe95ef6330f9f192ebb159a918a4
caroline <caroline-pierson@live.be>
2019-02-11 15:27:41 +0100

    Create the meal card component
    This component will contain a meal overview.

---
+++ src/app/mealCardComponent/mealCardComponent.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meal-card',
+  templateUrl: './mealCardComponent.html',
+  styleUrls: ['./mealCardComponent.scss'],
+})
+export class mealcardComponent { }
