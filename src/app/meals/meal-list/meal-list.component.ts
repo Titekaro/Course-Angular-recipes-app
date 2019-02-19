6d70a48fd6eb6b7524ba2299a7dd9e962e2338e1
caroline <caroline-pierson@live.be>
2019-02-19 12:13:16 +0100

    Add basic 'meal' model to the meal-list component

--- src/app/meals/meal-list/meal-list.component.ts
+++ src/app/meals/meal-list/meal-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+// Import your meal model
+import { meal } from '../meal.model';
+
 @Component({
   selector: 'app-meal-list',
   templateUrl: './meal-list.component.html',
   styleUrls: ['./meal-list.component.scss']
 })
 export class MealListComponent implements OnInit {
+  //Tell that meals array should only contain some meal model you created and imported
+  meals: meal[] = [];
 
   constructor() { }
 
