ba67b240196be949786db65a51a1259a90b32d62
caroline <caroline-pierson@live.be>
2019-03-27 17:13:38 +0100

    Add ingredients in the meal recipe component

--- src/app/meals/meal.service.ts
+++ src/app/meals/meal.service.ts
@@ -5,9 +5,22 @@ import {MealItem} from "./meal.model";
 export class MealService {
   // We define our array with our datas for meals, using the meal model.
   private meals: MealItem[] = [
-    new MealItem('Chili Con Carne', './assets/images/mexican/chili-con-carne.jpg', 45),
-    new MealItem('Tuna Poke Tacos', './assets/images/mexican/tuna-tacos.jpg', 40),
-    new MealItem('Kimchi & Bulgogi nachos', './assets/images/mexican/kimchi-nachos.jpg', 25)
+    new MealItem('Chili Con Carne', './assets/images/mexican/chili-con-carne.jpg', 45, [
+      '6 Patates douces',
+      '600g de boeuf haché',
+      '400g Tomate concassée en conserve',
+      '400g Haricot rouge en boîte',
+      '1 Oignon',
+      '2 Piments rouges',
+      '1 Gousse d\'ail',
+      '1/2 bouquet Coriandre',
+      '8 c. à s. Huile d\'olive',
+      '1 c. à c. Cumin',
+      '150g Crème fraîche épaisse',
+      'Sel et poivre'
+    ]),
+    new MealItem('Tuna Poke Tacos', './assets/images/mexican/tuna-tacos.jpg', 40, ['quatre', 'cinq', 'six']),
+    new MealItem('Kimchi & Bulgogi nachos', './assets/images/mexican/kimchi-nachos.jpg', 25, ['sept', 'huit'])
   ];
 
   // We define an eventEmitter for when we select a meal (MealItem).
