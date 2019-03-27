f7a4dfcd01f33a5cfbc105a2b951981ace6de6ac
caroline <caroline-pierson@live.be>
2019-03-27 18:08:37 +0100

    Add basic use of routing

---
+++ src/app/greek-meals/greek-meals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-greek-meals',
+  templateUrl: './greek-meals.component.html',
+  styleUrls: ['./greek-meals.component.scss']
+})
+export class GreekMealsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
