32c51ff2e478f66d9ac2c0a5ea1ff0240bcdb5f4
caroline <caroline-pierson@live.be>
2019-03-27 17:13:38 +0100

    Add ingredients in the meal recipe component

--- src/app/meals/meal.model.ts
+++ src/app/meals/meal.model.ts
@@ -1,7 +1,8 @@
 export class MealItem {
-  constructor(public name: string, public imagePath: string, public timeCooking: number){
+  constructor(public name: string, public imagePath: string, public timeCooking: number, public ingredients: any){
     this.name = name;
     this.imagePath = imagePath;
     this.timeCooking = timeCooking;
+    this.ingredients = ingredients;
   }
 }
