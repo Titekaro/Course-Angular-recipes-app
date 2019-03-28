cb77cfef130dbd71e2520ae8f4ec60e2c8d8bd6a
caroline <caroline-pierson@live.be>
2019-03-28 16:27:33 +0100

    Refactor meals component with a child route example

--- src/app/meals/meal-list/meal-list.component.ts
+++ src/app/meals/meal-list/meal-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MealService} from "../meal.service";
 import {MealItem} from "../meal.model";
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-meal-list',
@@ -10,13 +11,21 @@ import {MealItem} from "../meal.model";
 export class MealListComponent implements OnInit {
   // The list needs our meals
   meals: MealItem[];
+  mealsType: {type: string};
 
   // We call our service that contains the meals datas
-  constructor(private mealService: MealService) { }
+  constructor(private mealService: MealService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     // We tell that our array of meals is equal to the initialised array of meals on our service.
     this.meals = this.mealService.getMeals();
+
+    this.mealsType = {
+      type: this.route.snapshot.params['type']
+    };
+    this.route.params.subscribe((params: Params) => {
+      this.mealsType.type = params['type'];
+    });
   }
 
 }
