2d022a2303ef773b33773defe5816c6827c4a9f5
caroline <caroline-pierson@live.be>
2019-03-29 11:22:12 +0100

    Get the mealItems related to the route param (this doesn't work)

--- src/app/meals/meal.service.ts
+++ src/app/meals/meal.service.ts
@@ -4,24 +4,38 @@ import {MealItem} from "./meal.model";
 
 export class MealService {
   // We define our array with our datas for meals, using the meal model.
-  private meals: MealItem[] = [
-    new MealItem('Chili Con Carne', './assets/images/mexican/chili-con-carne.jpg', 45, [
-      '6 Patates douces',
-      '600g de boeuf haché',
-      '400g Tomate concassée en conserve',
-      '400g Haricot rouge en boîte',
-      '1 Oignon',
-      '2 Piments rouges',
-      '1 Gousse d\'ail',
-      '1/2 bouquet Coriandre',
-      '8 c. à s. Huile d\'olive',
-      '1 c. à c. Cumin',
-      '150g Crème fraîche épaisse',
-      'Sel et poivre'
-    ]),
-    new MealItem('Tuna Poke Tacos', './assets/images/mexican/tuna-tacos.jpg', 40, ['quatre', 'cinq', 'six']),
-    new MealItem('Kimchi & Bulgogi nachos', './assets/images/mexican/kimchi-nachos.jpg', 25, ['sept', 'huit'])
-  ];
+  private meals = {
+    'mexican': [
+      new MealItem('Chili Con Carne', './assets/images/mexican/chili-con-carne.jpg', 45, [
+        '6 Patates douces',
+        '600g de boeuf haché',
+        '400g Tomate concassée en conserve',
+        '400g Haricot rouge en boîte',
+        '1 Oignon',
+        '2 Piments rouges',
+        '1 Gousse d\'ail',
+        '1/2 bouquet Coriandre',
+        '8 c. à s. Huile d\'olive',
+        '1 c. à c. Cumin',
+        '150g Crème fraîche épaisse',
+        'Sel et poivre'
+      ]),
+      new MealItem('Tuna Poke Tacos', './assets/images/mexican/tuna-tacos.jpg', 40, ['quatre', 'cinq', 'six']),
+      new MealItem('Kimchi & Bulgogi nachos', './assets/images/mexican/kimchi-nachos.jpg', 25, ['sept', 'huit'])
+    ],
+    'greek': [
+      new MealItem('Saganaki', './assets/images/greek/sanagaki.jpg', 10, [
+        '1 fromage saganaki',
+        'Farine',
+        'Huile d\'olive',
+        '1/2 citron'
+      ]),
+      new MealItem('Moussaka', './assets/images/greek/moussaka.jpg', 25, ['sept', 'huit'])
+    ],
+    'italian': [
+      new MealItem('Saltimbocca', './assets/images/italian/saltimbocca.jpg', 35, ['sept', 'huit'])
+    ]
+  };
 
   // We define an eventEmitter for when we select a meal (MealItem).
   selectedMeal = new EventEmitter<MealItem>();
@@ -29,7 +43,7 @@ export class MealService {
   constructor() { }
 
   // We return our array of meals
-  getMeals() {
-    return this.meals.slice();
+  getMeals(type: string) {
+    return this.meals[type];
   }
 }
