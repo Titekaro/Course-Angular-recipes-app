282de362e58ec6bb0ed66d650e8baff88bbce988
caroline <caroline-pierson@live.be>
2021-01-14 22:03:16 +0100

    :hatching_chick: Create new sign in/sign out behavior to access a dashboard

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,32 @@
-import {Component} from "@angular/core";
+import {Component, OnDestroy, OnInit} from "@angular/core";
+import {UserService} from "../services/user.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
+  adminMode: boolean = false;
+  adminModeSubscription: Subscription;
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.adminModeSubscription = this.userService.adminMode.subscribe((adminMode: boolean) => {
+      this.adminMode = adminMode;
+    });
+    this.adminMode = !!localStorage.getItem('adminMode');
+  }
+
+  ngOnDestroy() {
+    this.adminModeSubscription.unsubscribe();
+  }
+
+  signOut() {
+    this.adminMode = false;
+    this.userService.onSignOut();
+  }
 }
