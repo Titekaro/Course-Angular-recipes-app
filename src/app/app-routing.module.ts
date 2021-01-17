5ba63162b4cb2355e0144cfdabb1ee6b46c18a37
caroline <caroline-pierson@live.be>
2021-01-17 19:55:02 +0100

    :sparkles: Add guard to access authentication only if not logged

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import {MealListComponent} from "./meal-list/meal-list.component";
 import {MealRecipeComponent} from "./meal-recipe/meal-recipe.component";
 import {AuthenticationComponent} from "./authentication/authentication.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {AuthenticationGuard} from "./guards/authentication/authentication.guard";
+import {DashboardGuard} from "./guards/dashboard/dashboard.guard";
 
 const appRoutes: Routes = [
   {path: '', component: MealsComponent},
@@ -13,7 +15,7 @@ const appRoutes: Routes = [
     {path: ':category', component: MealListComponent},
     {path: ':category/:name', component: MealRecipeComponent}
   ]},
-  {path: 'authentication', component: AuthenticationComponent},
+  {path: 'authentication', component: AuthenticationComponent, canActivate: [AuthenticationGuard]},
   {path: 'dashboard', component: DashboardComponent, canActivate: [DashboardGuard]},
   {path: '**', redirectTo: ''}
 ];
---
+++ src/app/guards/authentication/authentication.guard.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthenticationService} from "../../services/authentication/authentication.service";
+import {map, take} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationGuard implements CanActivate {
+
+  constructor(private router: Router, private authenticationService: AuthenticationService) {
+  }
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+    Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    /*We subscribe to our service to check if we got a user, but to emit data only once, we will use the take(1) method*/
+    return this.authenticationService.user.pipe(take(1), map(user => {
+      const isAuthenticated = !!user;
+      if (!isAuthenticated) {
+        return true;
+      } else {
+        return this.router.createUrlTree(['/']);
+      }
+    }));
+  }
+}
