7fe7ca683944d83fe4b526d8797be1d71a238463
caroline <caroline-pierson@live.be>
2021-01-27 16:46:17 +0100

    :ok_hand: Add missing unsubscribe from subscription in 'meal-list' component

--- src/app/meal-list/meal-list.component.ts
+++ src/app/meal-list/meal-list.component.ts
@@ -1,17 +1,19 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {MealService} from "../services/meal/meal.service";
 import {RecipeModel} from "../models/recipe.model";
 import {ActivatedRoute, Params} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-meal-list',
   templateUrl: './meal-list.component.html',
   styleUrls: ['./meal-list.component.scss']
 })
-export class MealListComponent implements OnInit {
+export class MealListComponent implements OnInit, OnDestroy {
   @Input() editMode = false;
   meals: RecipeModel[];
   mealOrigin: string;
+  mealChangedSubscription: Subscription;
 
   constructor(private route: ActivatedRoute, private mealService: MealService) {
   }
@@ -22,9 +24,13 @@ export class MealListComponent implements OnInit {
       this.meals = this.mealService.getMeals(this.mealOrigin);
     });
 
-    this.mealService.mealsChanged.subscribe((meals: RecipeModel[]) => {
+    this.mealChangedSubscription = this.mealService.mealsChanged.subscribe((meals: RecipeModel[]) => {
       this.meals = meals;
     });
+  }
+
+  ngOnDestroy() {
+    this.mealChangedSubscription.unsubscribe();
   }
 
 }
