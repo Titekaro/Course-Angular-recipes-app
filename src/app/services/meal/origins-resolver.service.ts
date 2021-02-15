ca010ee6b1652600529d840f7b3d31eea4c888e9
caroline <caroline-pierson@live.be>
2021-02-15 19:37:27 +0100

    :rotating_light: Fix multiplication of same data in origins resolver

--- src/app/services/meal/origins-resolver.service.ts
+++ src/app/services/meal/origins-resolver.service.ts
@@ -10,12 +10,14 @@ import {HttpClient} from "@angular/common/http";
 })
 export class OriginsResolverService implements Resolve<any[]> {
   private apiUrl = 'https://recipes-app-1f8a7-default-rtdb.europe-west1.firebasedatabase.app/';
-  origins = [];
+  origins: any[];
 
   constructor(private mealService: MealService, private http: HttpClient) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any[]> | Promise<any[]> | any[] {
+    this.origins = [];
+
     return this.http.get(this.apiUrl + 'origins.json').pipe(
       map(origins => {
         for (const key in origins) {
