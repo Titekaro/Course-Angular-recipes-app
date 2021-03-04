75deff1f6503ff4b5447eb530a1866e80c61dcce
caroline <caroline-pierson@live.be>
2021-03-04 16:27:57 +0100

    :rotating_light: Fix dashboard guard issue after modules refactor

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 
 import {DashboardComponent} from "./dashboard.component";
-import {DashboardGuard} from "../guards/dashboard/dashboard.guard";
 import {DashboardHomeComponent} from "./dashboard-home/dashboard-home.component";
 import {DashboardRecipesComponent} from "./dashboard-recipes/dashboard-recipes.component";
 import {FormRecipeComponent} from "../forms/form-recipe/form-recipe.component";
@@ -11,7 +10,7 @@ import {OriginsResolverService} from "../services/meal/origins-resolver.service"
 
 const dashboardRoutes: Routes = [
   {
-    path: '', component: DashboardComponent, canActivate: [DashboardGuard], children: [
+    path: '', component: DashboardComponent, children: [
       {path: 'home', component: DashboardHomeComponent},
       {path: 'recipes', component: DashboardRecipesComponent},
       {path: 'recipes/add', component: FormRecipeComponent, resolve: {
