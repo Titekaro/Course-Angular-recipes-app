c2e08df24317c6a2d6c2c9182a5f7a24d722aeaa
Caroline Pierson <caro.dcode@gmail.com>
2024-02-03 15:40:31 +0100

    :rotating_light: Fix missing services providers declaration for dashboard view in app.module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,29 @@ import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ViewportScroller} from '@angular/common';
 
 import {AppComponent} from './app.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {MealsComponent} from './meals/meals.component';
 import {HeaderComponent} from './header/header.component';
 import {AuthenticationComponent} from './authentication/authentication.component';
+import { NavTabsComponent } from './navbar/nav-tabs/nav-tabs.component';
+
 import {SharedModule} from './shared/shared.module';
 import {MealModule} from './meal-list/meal.module';
 import {RecipeModule} from './recipe/recipe.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { NavTabsComponent } from './navbar/nav-tabs/nav-tabs.component';
+
 import {AuthenticationService} from './services/authentication/authentication.service';
 import {UserService} from './services/user/user.service';
 import {MealService} from './services/meal/meal.service';
-import {AuthenticationGuard} from './guards/authentication/authentication.guard';
 import {MealResolverService} from './services/meal/meal-resolver.service';
-import {ViewportScroller} from '@angular/common';
+import {MealsResolverService} from './services/meal/meals-resolver.service';
+import {StatsService} from './services/stats/stats.service';
 
+import {AuthenticationGuard} from './guards/authentication/authentication.guard';
+import {DashboardGuard} from './guards/dashboard/dashboard.guard';
 
 @NgModule({
   declarations: [
@@ -43,12 +48,15 @@ import {ViewportScroller} from '@angular/common';
     SharedModule,
   ],
   providers: [
-    AuthenticationService,
     AuthenticationGuard,
+    DashboardGuard,
+    AuthenticationService,
     UserService,
     MealService,
     MealResolverService,
-    ViewportScroller,
+    MealsResolverService,
+    StatsService,
+    { provide: ViewportScroller, useClass: ViewportScroller, useValue: ViewportScroller }
   ],
   bootstrap: [AppComponent],
 })
